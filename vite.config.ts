333a270cebfefb683163c06d668c0ae587760973
Alexey Poltoradnev <poltoradnevone@gmail.com>
2025-01-07 12:29:35 +0400

    fix: configured env variables for bundle analyzer to work

--- vite.config.ts
+++ vite.config.ts
@@ -3,44 +3,52 @@ import { defineConfig } from "vite";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import "dotenv/config";
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 8080,
-  },
-  plugins: [
-    react({
-      babel: {
-        configFile: true,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }),
-    codecovVitePlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "@codecov/vite-plugin",
-      uploadToken: process.env.CODECOV_TOKEN,
-    }),
-  ],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    include: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-    coverage: {
-      include: [
-        "**/src/**/*.[jt]s?(x)",
-        "**/src/**/?(*.)+(spec|test).[jt]s?(x)",
-        "!**/src/main.tsx",
-      ],
-      provider: "v8",
-      reporter: ["text", "html", "clover", "json", "json-summary"],
     },
-  },
+    server: {
+      port: 3000,
+    },
+    preview: {
+      port: 8080,
+    },
+    plugins: [
+      react({
+        babel: {
+          configFile: true,
+        },
+      }),
+      codecovVitePlugin({
+        enableBundleAnalysis: isProduction,
+        bundleName: "@codecov/vite-plugin",
+        uploadToken: process.env.CODECOV_TOKEN,
+      }),
+    ],
+    test: {
+      environment: "jsdom",
+      globals: true,
+      include: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)",
+      ],
+      coverage: {
+        include: [
+          "**/src/**/*.[jt]s?(x)",
+          "**/src/**/?(*.)+(spec|test).[jt]s?(x)",
+          "!**/src/main.tsx",
+        ],
+        provider: "v8",
+        reporter: ["text", "html", "clover", "json", "json-summary"],
+      },
+    },
+  };
 });
