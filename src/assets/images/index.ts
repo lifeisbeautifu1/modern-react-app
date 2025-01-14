62488f0a4f63b595e0041c30e7bc633903b5ef52
Alexey Poltoradnev <poltoradnevone@gmail.com>
2025-01-14 21:12:43 +0400

    feat: almost done with home page
    almost done with home page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,9 @@
+export { default as CallToActionDesktopPng } from "./cta-desktop.png";
+export { default as CallToActionDesktopWebp } from "./cta-desktop.webp";
+export { default as CallToActionMobilePng } from "./cta-mobile.png";
+export { default as CallToActionMobileWebp } from "./cta-mobile.webp";
+export { default as CallToActionTabletPng } from "./cta-tablet.png";
+export { default as CallToActionTabletWebp } from "./cta-tablet.webp";
 export { default as HeroImagePng } from "./hero-image.png";
 export { default as HeroImageWebp } from "./hero-image.webp";
 export { default as ImagePng } from "./image.png";
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,13 +1,28 @@
 import type { ComponentProps } from "react";
 import { cn } from "@/utils";
 
-export const Button = (props: ComponentProps<"button">) => {
+interface ButtonProps {
+  /** Button appearance. @default "on-light" */
+  appearance?: "on-light" | "on-dark";
+}
+
+export const Button = ({
+  appearance = "on-light",
+  className,
+  ...restProps
+}: ComponentProps<"button"> & ButtonProps) => {
   return (
     <button
-      {...props}
+      {...restProps}
       className={cn(
-        "rounded-[32px] py-3 px-6 font-bold bg-black text-white hover:bg-black/90 active:bg-black/80 transition focus-visible:ring-4 focus-visible:outline-none tracking-[0.1px]",
-        props.className,
+        "rounded-[32px] py-3 px-6 font-bold transition focus-visible:ring-4 focus-visible:outline-none tracking-[0.1px]",
+        {
+          "bg-black text-white hover:bg-black/90 active:bg-black/80":
+            appearance === "on-light",
+          "bg-white text-black hover:bg-white/90 active:bg-white/80":
+            appearance === "on-dark",
+        },
+        className,
       )}
     />
   );
