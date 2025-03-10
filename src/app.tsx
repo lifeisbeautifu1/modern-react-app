e4222a09313118cbf38ff7873e40d8ed62e02c33
Alexey Poltoradnev <poltoradnevone@gmail.com>
2025-03-10 16:07:10 +0400

    feat: test page

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,28 @@
-import { Suspense, lazy } from "react";
+import { Fragment, Suspense, lazy } from "react";
 import { Routes, Route } from "react-router";
-import { Layout } from "@/components";
+// import { Layout } from "@/components";
 
 const Home = lazy(() => import("@/pages/home"));
 const NotFound = lazy(() => import("@/pages/404"));
+
+const Test = () => {
+  return (
+    <Fragment>
+      <button>Native button</button>
+      <p style={{ fontSize: "16px" }}>Some text here</p>
+      <p style={{ fontSize: "1rem" }}>Some text here</p>
+      <p style={{ fontSize: "1em" }}>Some text here</p>
+    </Fragment>
+  );
+};
 
 export function App() {
   return (
     <Suspense fallback={null}>
       <Routes>
-        <Route element={<Layout />}>
+        <Route>
           <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
