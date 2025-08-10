ba2c5e9eaa2b8d32cb93bb44dd778d1bfddd31eb
Alexey Poltoradnev <poltoradnevone@gmail.com>
2025-08-10 19:09:19 +0400

    perf: moved all images to CDN

--- src/pages/home/constants.ts
+++ src/pages/home/constants.ts
@@ -2,21 +2,24 @@ export const TESTIMONIALS = [
   {
     fullName: "Name Surname",
     description: "Description",
-    avatarUrl: "https://eu.ui-avatars.com/api/?name=Name+Surname&size=64",
+    avatarUrl:
+      "https://res.cloudinary.com/dvzjexy5g/image/upload/c_fit,h_64,w_64/v1754837702/avatar_e5afyv.png",
     testimonial:
       "“A testimonial describing what the person thinks about this service, product or startup in general.”",
   },
   {
     fullName: "Name Surname",
     description: "Description",
-    avatarUrl: "https://eu.ui-avatars.com/api/?name=Name+Surname&size=64",
+    avatarUrl:
+      "https://res.cloudinary.com/dvzjexy5g/image/upload/c_fit,h_64,w_64/v1754837702/avatar_e5afyv.png",
     testimonial:
       "“A testimonial describing what the person thinks about this service, product or startup in general.”",
   },
   {
     fullName: "Name Surname",
     description: "Description",
-    avatarUrl: "https://eu.ui-avatars.com/api/?name=Name+Surname&size=64",
+    avatarUrl:
+      "https://res.cloudinary.com/dvzjexy5g/image/upload/c_fit,h_64,w_64/v1754837702/avatar_e5afyv.png",
     testimonial:
       "“A testimonial describing what the person thinks about this service, product or startup in general.”",
   },
